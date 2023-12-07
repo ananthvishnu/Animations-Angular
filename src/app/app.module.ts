4b23da2a59c5b041d3c6a59f591349f666f42409
Ananthakumar Vishnurathan <124557188+ananthvishnu@users.noreply.github.com>
2023-12-07 10:43:15 +0530

    lottie animation logo added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+
+// Add these two
+import { LottieModule } from 'ngx-lottie';
+import player from 'lottie-web';
+
+// Export this function
+export function playerFactory(): any {  
+  return import('lottie-web');
+}
 
 @NgModule({
   declarations: [
@@ -14,7 +24,9 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+      // Add the module like so:    
+      LottieModule.forRoot({ player: playerFactory }), 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  // This is the option that uses the package's AnimationOption interface  
+  options: AnimationOptions = {    
+    path: '../../../assets/Lottie\ Lego.json'  
+  };  
 
+  constructor() { }  
+
+  ngOnInit(): void {  }
+
+  // This is the component function that binds to the animationCreated event from the package  
+  onAnimate(animationItem: AnimationItem): void {    
+    console.log(animationItem);  
+  }
 }
